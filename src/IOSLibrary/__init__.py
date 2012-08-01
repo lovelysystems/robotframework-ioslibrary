55beacaf69c8fa31510408e0288499735cb30775
Philipp Bogensberger <bogensberger@lovelysystems.com>
2012-08-01 11:31:56 +0200

    added scroll, swipe, rotate and pinch

--- src/IOSLibrary/__init__.py
+++ src/IOSLibrary/__init__.py
@@ -3,8 +3,23 @@ import logging
 import json
 import os
 import robot
+import time
 from robot.variables import GLOBAL_VARIABLES
 from robot.api import logger
+
+ORIENTATIONS = {
+    "down":0,
+    "right":90,
+    "left":270,
+    "up":180
+}
+
+ORIENTATIONS_REV = {
+    0:"down",
+    90:"right",
+    180:"up",
+    270:"left"
+}
 
 class IOSLibrary(object):
 
@@ -13,6 +28,7 @@ class IOSLibrary(object):
     def __init__(self, device_endpoint='localhost:37265'):
         self._url = 'http://%s/' % device_endpoint
         self._screenshot_index = 0
+        self._current_orientation = 0
 
     def post(self, endp, request):
         logging.info("Request to device %s: %s", self._url+endp, request)
@@ -32,7 +48,7 @@ class IOSLibrary(object):
 
     def _map(self, query, method_name, args=[]):
         data = json.dumps({
-            "query":json.loads(query),
+            "query":query,
             "operation":{
                 "arguments":args,
                 "method_name":method_name
@@ -44,9 +60,17 @@ class IOSLibrary(object):
             self._screen_and_raise('map %s failed because: %s \n %s' % (query, res['reason'], res['details']))
         return res['results']
 
+    def _screenshot(self, filename=None):
+        res = self.get('screenshot')
+        path, link = self._get_screenshot_paths(filename)
+        with open(path,'w') as f:
+            f.write(res.content)
+        logger.info('</td></tr><tr><td colspan="3"><a href="%s">'
+                   '<img src="%s"></a>' % (link, link), True, False)
+
     def _screen_and_raise(self, err):
         logger.warn(err)
-        self.capture_screenshot()
+        self._screenshot()
         raise Exception
 
     def _load_playback_data(self, recording, options=None):
@@ -68,13 +92,50 @@ class IOSLibrary(object):
         post_data = {
             "events": data
         }
-        if options['query']:
-            post_data['query']=options['query']
+        if options:
+                if options.has_key('query'):
+                    post_data['query']=options['query']
+                if options.has_key('offset'):
+                    post_data['offset']=options['offset']
+                if options.has_key('reverse'):
+                    post_data['reverse']=options['reverse']
+                if options.has_key('prototype'):
+                    post_data['prototype']=options['prototype']
         res = json.loads(self.post('play',json.dumps(post_data)).text)
         if res['outcome'] != 'SUCCESS':
             self._screen_and_raise('playback failed because: %s \n %s' % (res['reason'],res['details']))
         return res['results']
         
+    def _rotate_to(self, orientation, direction="left"):
+        orientation = self._reduce_degrees(orientation)
+        self._current_orientation = orientation
+        if direction == "right":
+           orientation +=90
+        elif direction == "left":
+           orientation +=270
+        orientation = self._reduce_degrees(orientation)
+        orientation = ORIENTATIONS_REV[orientation]
+        playback = "rotate_%s_home_%s" % (direction,orientation)
+        self._playback(playback)
+
+    def _reduce_degrees(self, degrees):
+        while degrees >=360:
+            degrees -=360
+        while degrees < 0:
+            degrees +=360
+        return degrees
+
+    def query(self, query):
+        return self._map(query,"query")
+
+    def query_all(self, query):
+        return self._map(query,"query_all")
+
+    def _pinch(self, in_out, options={}):
+        f = "pinch_in"
+        if in_out == "out":
+            f = "pinch_out"
+        self._playback(f, options)
 
     # BEGIN: STOLEN FROM SELENIUM2LIBRARY
 
@@ -99,14 +160,16 @@ class IOSLibrary(object):
 
     # DEFINITIONS
 
-    def query(self, query):
-        return self._map(query,"query")
-
-    def query_all(self, query):
-        return self._map(query,"query_all")
-
     def touch(self, query):
         self._playback("touch",{"query":query})
+
+    def touch_position(self, x=0, y=0):
+        self._playback("touch",
+                    {"offset":{
+                        "x":x,
+                        "y":y
+                        }
+                    })
 
     def check_element_does_exist(self,query):
         assert self.search_ui_elements(query)["results"]
@@ -114,11 +177,55 @@ class IOSLibrary(object):
     def check_element_does_not_exist(self,query):
         assert not self.search_ui_elements(query)["results"]
 
-    def capture_screenshot(self, filename=None):
-        res = self.get('screenshot')
-        path, link = self._get_screenshot_paths(filename)
-        with open(path,'w') as f:
-            f.write(res.content)
-        logger.info('</td></tr><tr><td colspan="3"><a href="%s">'
-                   '<img src="%s"></a>' % (link, link), True, False)
-    # DEFINITIONS
+    def capture_screenshot(self,filename=None):
+        self._screenshot(filename)
+
+    def toggle_switch(self, name=None):
+        if not name:
+            self.touch("switch")
+        else:
+            self.touch("switch marked:'%s'" % name)
+
+    def touch_text(self, name=None):
+        if not name:
+            self.touch("textField")
+        else:
+            self.touch("textField placeholder:'%s'" % name)
+
+    def go_back(self):
+        self.touch("navigationItemButtonView first")
+
+    def enter_background(self, time=10):
+        self.post('background',json.dumps({'duration':time}))         
+
+    def rotate(self, direction):
+        if direction == "right":
+            self._current_orientation -= 90
+        elif direction == "left":
+            self._current_orientation += 90
+        else:
+            self._screen_and_raise("not a valid direction %s" % direction)
+        self._rotate_to(self._current_orientation, direction)
+
+    def set_device_orientation_to(self, orientation, direction="left"):
+        degrees = ORIENTATIONS[orientation]
+        self._rotate_to(degrees, direction)
+
+    def scroll(self, direction, query = "scrollView index:0"):
+        views_touched = self._map(query, "scroll", [direction])
+        if not views_touched:
+            self._screen_and_raise("could not find a view to scroll: %s" % query)
+    def pinch(self, direction, query = None):
+        options = {}
+        if query:
+           options = {"query":query} 
+        self._pinch(direction, options)
+
+    def swipe(self, direction):
+        degrees = ORIENTATIONS[direction]
+        direction = (360 - self._current_orientation) + degrees
+        direction = self._reduce_degrees(direction)
+        direction = ORIENTATIONS_REV[direction]
+        self._playback("swipe_%s" % direction)
+
+    # END: DEFINITIONS
